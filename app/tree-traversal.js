b40b1c2945b46c4ece957f1686ad678ad7287bb3
JavaScript software engineer <pure-js@users.noreply.github.com>
2017-08-23 19:56:33 +0300

    Add JSDoc

--- app/tree-traversal.js
+++ app/tree-traversal.js
@@ -8,12 +8,19 @@ const folder = 'img[src="/businesspublisher/layouts/default/images/tree/folder.g
  */
 const isLink = (id) => document.querySelector(`#${id} a`);
 
-// Click if its possible, if not - find the next one
+/**
+ * Click if its possible, if not - find the next one
+ * @param {string} id
+ */
 const tryClick = (id) => {
   isLink(id) ? getNext(id) : doClick(id);
 };
 
-// Wait for server response and html rendering
+/**
+ * Wait for server response and html rendering
+ * @param {string} id
+ * @param {number} delay
+ */
 const timeoutSetter = (id, delay = 60) => {
   setTimeout(function () {
     let next = document.querySelector('#' + id + ' nobr');
@@ -21,14 +28,21 @@ const timeoutSetter = (id, delay = 60) => {
   }, delay);
 };
 
-// Do click on image in element
+/**
+ * Do click on image in element
+ * @param {string} id
+ */
 const doClick = (id) => {
   document.querySelector(`#${id} folder`).click();
   console.log('click on', id);
   timeoutSetter(id, 100);
 };
 
-// Get the next one for the parent one
+/**
+ * Get the next one for the parent one
+ * @param {string} id
+ * @returns {Element}
+ */
 const getNextForParent = (id) => {
   let current = document.getElementById(id);
   if (current) {
@@ -38,7 +52,10 @@ const getNextForParent = (id) => {
   }
 };
 
-// Find next id
+/**
+ * Find next id
+ * @param {string} id
+ */
 const getNext = (id) => {
   // get the next one
   let next = document.getElementById(id).nextSibling;
